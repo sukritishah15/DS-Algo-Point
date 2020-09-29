6ad145a5d1bcefff6a9d6548730792d39f55ae2e
chhipanikhil9 <60360461+chhipanikhil9@users.noreply.github.com>
2020-09-30 02:30:29 +0530

    Add program to check whether the given expression is blanced or not.

---
+++ C-Plus-Plus/balance_paranthesis.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool balance(string s){
+    stack<char> st;
+    for(int i=0;i<s.size();i++){
+        if(s[i] == '{' || s[i]=='(' || s[i]=='['){
+            st.push(s[i]);
+        }
+        else if(st.top()=='{' && s[i]=='}'){
+            st.pop();
+        }
+        else if(st.top()=='(' && s[i]==')'){
+            st.pop();
+        }
+        else if(st.top()=='[' && s[i]==']'){
+            st.pop();
+        }
+        else{
+            break;
+        }
+    }
+    if(!st.empty()){
+        return false;
+    }
+    else{
+        return true;
+    }
+}
+
+int main(){
+    string s;cin>>s;
+    if(balance(s)){
+        cout<<"Balanced"<<endl;
+    }
+    else{
+        cout<<"Not Balanced"<<endl;
+    }
+    return 0;
+}
