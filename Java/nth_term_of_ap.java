288f8ceff942e7625dae995f20790647fff838b3
Manan Chawla <42414965+mananchawla2005@users.noreply.github.com>
2020-09-30 21:20:04 +0530

    Update nth_term_of_ap.java

--- Java/nth_term_of_ap.java
+++ Java/nth_term_of_ap.java
@@ -11,13 +11,15 @@ public class nth_term_of_ap {
         int n = 5;
         System.out.println(t1+((n-1)*(t2-t1)));
         
-        // sample input 
-        // Let the first term be 2 and second term be 5. Also let the value of n be 5
-        // The programs calculates the nth term by using formula of ap i.e. a+ (n-1)d
-        // Output is 14.
+        /* sample input 
+         Let the first term be 2 and second term be 5. Also let the value of n be 5
+         The programs calculates the nth term by using formula of ap i.e. a+ (n-1)d
+         Output is 14. 
+         This code is contributed by Manan Chawla
+         Time Complexity is O(1) and space complexity is O(1)
+         */
         
 
     }
 }
-// This code is contributed by Manan Chawla
-// Time Complexity is O(1) and space complexity is O(1)
+
