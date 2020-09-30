46117207cfdbbe3169be75a91e14c3b35a766dc8
Shaswat Singh <45266507+shaswatsingh19@users.noreply.github.com>
2020-09-30 20:46:13 +0530

    Update Sieve_of_Eratosthenes.py

--- Python/Sieve_of_Eratosthenes.py
+++ Python/Sieve_of_Eratosthenes.py
@@ -2,8 +2,8 @@ import math
 # taking input 
 n  = int(input('Write a number\n'))
 
-# making a list named primes initialize primes with 1 only 
-# here 1 represent the value is prime and we believe all value are prime initially
+'''making a list named primes initialize primes with 1 only 
+here 1 represent the value is prime and we believe all value are prime initially'''
 primes = [1]*(n+1)
 
 # making first and second values as 0 as  0, 1 is not a prime and start by checking 
@@ -11,10 +11,12 @@ primes = [1]*(n+1)
 primes[0] = 0
 primes[1] = 0
 
-# looping through 2,sqrt(n)+1 as every number 
-# comes with 2 factor if first factor is seen then
-# we don't have to look for second one
-# in second loop we are initialize all multiple of 
+'''
+looping through 2,sqrt(n)+1 as every number 
+comes with 2 factor if first factor is seen then
+we don't have to look for second one
+in second loop we are initialize all multiple of 
+'''
 # i in first loop
 for i in range(2, int( math.sqrt(n)+1 ) ):
     if primes[i] == 1:
