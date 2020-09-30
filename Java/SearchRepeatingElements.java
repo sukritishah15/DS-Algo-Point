47edf61169b6d85c3012b85a370d15c245db09b3
garv wadhwa <54171077+garv-cmd@users.noreply.github.com>
2020-09-30 22:27:54 +0530

    Create SearchRepeatingElements.java

---
+++ Java/SearchRepeatingElements.java
@@ -0,0 +1,16 @@
+package searching;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SearchRepeatingElements {
+    public static void main(String args[]){
+        int arr[]={1,2,2,5,7,4,5,8,7};
+        Set<Integer> set = new HashSet<>();
+        for(int i=0;i<arr.length;i++){
+            if(!set.add(arr[i])){
+                System.out.print(arr[i]+" ");
+            }
+        }
+    }
+}
