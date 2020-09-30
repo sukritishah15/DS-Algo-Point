760ed10d84111c0909eeca721a5973688a92fcef
mananapeejay <42414965+mananapeejay@users.noreply.github.com>
2020-09-30 20:35:57 +0530

    added code for nth term of ap for python

---
+++ Python/nth_term_of_ap.py
@@ -0,0 +1,10 @@
+t1 = 2
+t2 = 5
+n = 5
+print(t1+((n-1)*(t2-t1)))
+
+# sample input 
+# Let the first term be 2 and second term be 5. Also let the value of n be 5
+# The programs calculates the nth term by using formula of ap i.e. a+ (n-1)d
+# Output is 14.
+# Time Complexity is O(1) and space complexity is O(1)
