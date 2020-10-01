bc9f646be2bdf166667bff320992f58275133823
Sushant <56603153+sushant-varanasi@users.noreply.github.com>
2020-10-01 14:38:17 +0530

    Create GreedyAlgo.cpp

---
+++ C-Plus-Plus/GreedyAlgo.cpp
@@ -0,0 +1,36 @@
+// C++ program to find minimum number of denominations 
+#include <bits/stdc++.h> 
+using namespace std; 
+  
+// All denominations of Indian Currency 
+int deno[] = { 1, 2, 5, 10, 20, 50, 100, 500, 1000 }; 
+int n = sizeof(deno) / sizeof(deno[0]); 
+  
+// Driver program 
+void findMin(int V) 
+{ 
+    // Initialize result 
+    vector<int> ans; 
+  
+    // Traverse through all denomination 
+    for (int i = n - 1; i >= 0; i--) { 
+        // Find denominations 
+        while (V >= deno[i]) { 
+            V -= deno[i]; 
+            ans.push_back(deno[i]); 
+        } 
+    } 
+  
+    // Print result 
+    for (int i = 0; i < ans.size(); i++) 
+        cout << ans[i] << "  "; 
+} 
+  
+// Driver program 
+int main() 
+{ 
+    int n = 93; 
+    cout << "Following is minimal number of change for " << n << " is "; 
+    findMin(n); 
+    return 0; 
+} 
