430190804805a957a3a9664b15a4fd372950dec0
harshit-labs <61293877+harshit-labs@users.noreply.github.com>
2020-10-01 15:45:59 +0530

    Create Seive of Eratosthenes.c

---
+++ C/Seive of Eratosthenes.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void seive_of_era(int n){
+    long long i,j;
+    int *primes;
+    primes = malloc(sizeof(int) * (n+1));
+    for (i = 2;i<n+1; i++){
+        primes[i] = 1;
+    }
+
+    for (i = 2;i <n+1; i++){
+        if (primes[i]){
+            for (j=i;i*j<n+1; j++){
+                primes[i * j] = 0;
+            }
+        }
+    }
+    printf("Prime number less than or equal to are: \n");
+    for (i=2;i<n+1;i++){
+        if (primes[i]){
+            printf("%d ", i);
+        }
+    }
+}
+int main(void) {
+	int n;
+	printf("Enter the Number: ");
+	scanf("%d",&n);
+	seive_of_era(n);
+	return 0;
+}
+
