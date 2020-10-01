b785dc15b56d90f5a6c90b6b1fccd7fcd9173c05
Sushant <56603153+sushant-varanasi@users.noreply.github.com>
2020-10-01 14:46:26 +0530

    Update and rename GreedyAlgo.cpp to MinDenominations_GreedyAlgo.cpp

--- C-Plus-Plus/MinDenominations_GreedyAlgo.cpp
+++ C-Plus-Plus/MinDenominations_GreedyAlgo.cpp
@@ -1,4 +1,6 @@
-// C++ program to find minimum number of denominations 
+// C++ program to find minimum number of denominations
+/*Given a value V, if we want to make change for V Rs, and we have infinite supply of each of the denominations in Indian currency, i.e., we have infinite supply of { 1, 2, 5, 10, 20, 50, 100, 500, 1000} valued coins/notes, what is the minimum number of coins and/or notes needed to make the change?
+*/
 #include <bits/stdc++.h> 
 using namespace std; 
   
@@ -34,3 +36,16 @@ int main()
     findMin(n); 
     return 0; 
 } 
+
+/*
+
+Complexity Analysis:
+
+Time Complexity: O(V).
+Auxiliary Space: O(1) as no additional space is used.
+
+
+Sample output: 
+Following is minimal number of change for 93 is 50  20  20  2  1
+
+*/
