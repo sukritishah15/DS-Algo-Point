d7396c6440f85c4aabc59589d0af7949727e6383
unknown <marnadmanavi92@gmail.com>
2020-10-02 14:59:59 +0530

    added ternary search in python

--- Python/ternary_search.py
+++ Python/ternary_search.py
@@ -59,8 +59,11 @@ Enter 4 elements
 1 3 2 4
 Enter the key to be found
 3
-3 is found at index 2"""
+3 is found at index 2
+time complexity O(logN)
+space complexity O(1)
 
-#time complexity O(logN)
-#space complexity O(1)
+"""
+
+
 
