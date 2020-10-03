edbb5bc35e700c30b27bdc51c35947d4b22c0ed0
Garima Mahajan <56437933+GarimaMahajan20@users.noreply.github.com>
2020-10-03 13:58:26 +0530

    Create preorder.c

---
+++ C/preorder.c
@@ -0,0 +1,64 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+
+/* A binary tree node has data, pointer to left child 
+   and a pointer to right child */
+struct node 
+{ 
+     int data; 
+     struct node* left; 
+     struct node* right; 
+}; 
+
+/* Helper function that allocates a new node with the 
+   given data and NULL left and right pointers. */
+struct node* newNode(int data) 
+{ 
+     struct node* node = (struct node*) 
+                                  malloc(sizeof(struct node)); 
+     node->data = data; 
+     node->left = NULL; 
+     node->right = NULL; 
+
+     return(node); 
+} 
+
+/* Given a binary tree, print its nodes in preorder*/
+void printPreorder(struct node* node) 
+{ 
+     if (node == NULL) 
+          return; 
+
+     /* first print data of node */
+     printf("%d ", node->data);   
+
+     /* then recur on left sutree */
+     printPreorder(node->left);   
+
+     /* now recur on right subtree */
+     printPreorder(node->right); 
+}     
+
+/* Driver program to test above functions*/
+int main() 
+{ 
+     struct node *root  = newNode(1); 
+     root->left             = newNode(2); 
+     root->right           = newNode(3); 
+     root->left->left     = newNode(4); 
+     root->left->right   = newNode(5);  
+
+     printf("\nPreorder traversal of binary tree is \n"); 
+     printPreorder(root); 
+
+     getchar(); 
+     return 0; 
+} 
+/*
+  Time Complexity - O(N)
+  Space Complexity - O(N)
+  
+  Input - Taken in the Code .
+  Output -Prerder traversal of binary tree is
+          1 2 4 5 3
+*/
