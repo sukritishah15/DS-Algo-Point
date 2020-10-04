79738fcb959d08f5cff83574f1d7a491e1a2c079
masterrachit777 <masterrachit777@gmail.com>
2020-10-04 22:09:06 +0530

    added sample I/O and time complexity

--- C#/TwinPrime.cs
+++ C#/TwinPrime.cs
@@ -50,5 +50,39 @@ public class GFG {
     }    
 }
 
-// OUTPUT
+// Sample INPUT
+// find twin prime between 1 - 25 
+//sample OUTPUT
 // (3, 5)(5, 7)(11, 13)(17, 19)
+
+/*
+Time Complexity:
+
+Inside the function :
+
+        bool[] prime = new bool[n + 1];     ------------O(1) 
+  
+        for (int i = 0; i < n + 1; i++)  ----------------O(n)
+            prime[i] = true; 
+  
+        for (int p = 2; p * p <= n; p++) { 
+            // If prime[p] is not changed, 
+            // then it is a prime 
+              
+            if (prime[p] == true) { 
+              
+                // Update all multiples of p 
+                for (int i = p * 2; i <= n; i += p) -------------O(n^2)
+                    prime[i] = false; 
+            } 
+        } 
+  
+        // check for twin prime numbers 
+        // To display th result 
+        for (int i = 2; i <= n - 2; i++) {      -----------------O(n)
+            if (prime[i] == true && prime[i + 2] == true) 
+                Console.Write(" (" + i + ", " + (i + 2) + ")"); 
+        } 
+
+Therefore total time = O(1) + O(n) + O(n^2) + O(n) = O(n^2)
+*/
