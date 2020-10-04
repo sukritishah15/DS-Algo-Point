b6beef0ab316b72a8fa6aa46b19044cab6dcce2b
shaunak56 <37690472+shaunak56@users.noreply.github.com>
2020-10-04 10:26:55 +0530

    Update FizzBuzz.py

--- Python/FizzBuzz.py
+++ Python/FizzBuzz.py
@@ -20,3 +20,16 @@ start_point = 1
 end_point = 100
 
 print_fizz_buzz(start_point, end_point)
+
+"""
+Function print_fizz_buzz:
+
+Input: 2 integers, start point and end point
+
+Output: prints the words Fizz, Buzz, FizzBuzz or integer itself 
+for each number between start and end point (inclusive) 
+
+Time complexity: O(N) (N is length of range of numbers)
+
+Space complexity: O(1) (Uses only constant memory for iterator from start to end)
+"""
