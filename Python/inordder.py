33f42b70a4aaebc851852dd2a92b0acfc20cfd7a
Garima Mahajan <56437933+GarimaMahajan20@users.noreply.github.com>
2020-10-04 21:56:21 +0530

    Update inordder.py

--- Python/inordder.py
+++ Python/inordder.py
@@ -3,24 +3,14 @@ class Node:
         self.left = None
         self.right = None
         self.val = key 
-
-
-# A function to do inorder tree traversal 
+        
 def printInorder(root): 
 
     if root: 
-
-        # First recur on left child 
         printInorder(root.left) 
-
-        # then print the data of node 
         print(root.val), 
-
-        # now recur on right child 
         printInorder(root.right) 
 
-
-# Driver code 
 root = Node(1) 
 root.left      = Node(2) 
 root.right     = Node(3) 
