9d3257c8a20684b6fba5db5d7f9e496d81bf3020
Siddhant Sarkar <siddhant.sarkar999@gmail.com>
2020-10-05 00:57:51 +0530

    The Trignometric Ratio Added

--- Java/Trigonometry.java
+++ Java/Trigonometry.java
@@ -5,6 +5,7 @@ public class Trigonometry {
 
     public static void main(String[] args) {
         boolean quit = false;
+        double temp=0;
         while (!quit == true) {
             printMenu();
             int option = scanner.nextInt();
@@ -12,23 +13,47 @@ public class Trigonometry {
 
             switch (option) {
                 case 1:
+                    temp =enter();
+                    System.out.println("***************************");
+                    System.out.println("Sine("+temp+") is "+Math.sin(temp));
+                    System.out.println("***************************");
                     break;
                 case 2:
+                    temp =enter();
+                    System.out.println("***************************");
+                    System.out.println("Cosine("+temp+") is "+Math.cos(temp));
+                    System.out.println("***************************");
                     break;
                 case 3:
+                    temp =enter();
+                    System.out.println("***************************");
+                    System.out.println("Tangent("+temp+") is "+Math.tan(temp));
+                    System.out.println("***************************");
                     break;
                 case 4:
+                    temp =enter();
+                    System.out.println("***************************");
+                    System.out.println("Tangent("+temp+") is "+1/Math.tan(temp));
+                    System.out.println("***************************");
                     break;
                 case 5:
+                    temp =enter();
+                    System.out.println("***************************");
+                    System.out.println("Tangent("+temp+") is "+1/Math.sin(temp));
+                    System.out.println("***************************");
                     break;
                 case 6:
+                    temp =enter();
+                    System.out.println("***************************");
+                    System.out.println("Tangent("+temp+") is "+1/Math.cos(temp));
+                    System.out.println("***************************");
                     break;
                 case 7:
                     quit = true;
+                    System.out.println("Thankyou  quiting now ");
                     break;
                 default:
                     System.out.println("Please enter withing the range");
-
             }
         }
     }
@@ -41,9 +66,15 @@ public class Trigonometry {
         System.out.println("Enter 2 to find Cosine");
         System.out.println("Enter 3 to find Tangent");
         System.out.println("Enter 4 to find Cotangent");
-        System.out.println("Enter 5 to find Secant");
-        System.out.println("Enter 6 to find Cosecant");
-        System.out.println("Enter 7 to quit");
+        System.out.println("Enter 5 to find Cosecant");
+        System.out.println("Enter 6 to find Secant");
+        System.out.println("Enter 7 to quit ");
 
+    }
+    public static double enter(){
+        System.out.println("Enter the angle in radians");
+        double angle=scanner.nextDouble();
+        scanner.nextLine();
+        return angle;
     }
 }
