6bb4c43c179d98262c3cbaf8a2c3075c0108bace
Hitansh K Doshi <hkd159@gmail.com>
2020-10-04 18:01:07 +0530

    In C folder created floyd-warshall.c also updated README.md.
    Also tested against 2 test cases one with directed and undirected
    graph results obtaind as expected.
    Changes Done mentioned in #420. put comment in end.

--- C/floyd-warshall.c
+++ C/floyd-warshall.c
@@ -1,66 +1,3 @@
-/*
-Folyd-warshall alorithm is a path finding algorithm.It finds shortest path between two nodes.
-Implementation:
-  Used adjacent matrix to store paths.
-  Used dynamic memory allocation for user input length array. 
-
-Input:
-  First line consist of a number of nodes (n) and number of edges (edges) and is graph directed or not (directed) (it accepts 0 or 1).
-  Followed by edges number of line consisting u v w where
-    u: start node
-    v: end node
-    w: weight of edge
-  Followed by a and b:
-    a: start node
-    b: end node
-
-Output:
-  adjacent matrix followed by 
-  Shortest distance between a and b.
-
-Sample input-output:
-  Directed graph
-  Input1:
-    4 5 1
-    1 2 5
-    2 3 7
-    2 4 6
-    3 4 5
-    4 1 2
-    3 2
-
-  Output1:
-    0 5 12 11
-    8 0 7 6
-    7 12 0 5
-    2 7 14 0
-    Shortest distance between 3 and 2 is 12
-
-  Undirected graph
-  Input2:
-    5 6 0
-    1 2 5
-    1 4 9
-    1 5 1
-    2 3 2
-    3 4 7
-    4 5 2
-    5 3
-
-  Output2:
-    0 5 7 3 1 
-    5 0 2 8 6 
-    7 2 0 7 8 
-    3 8 7 0 2 
-    1 6 8 2 0 
-    Shortest distance between 5 and 3 is 8
-
-  Time complexity = O (n^3)
-  Space complexity = O (n^2)
-
-  Contributed by: Hitansh K Doshi (github id: Hitansh159)
-*/
-
 #include<stdio.h>
 #include<stdlib.h>
 #define INF 1e9
@@ -141,3 +78,66 @@ int main(){
 
   return 0;
 }
+
+/*
+Folyd-warshall alorithm is a path finding algorithm.It finds shortest path between two nodes.
+Implementation:
+  Used adjacent matrix to store paths.
+  Used dynamic memory allocation for user input length array. 
+
+Input:
+  First line consist of a number of nodes (n) and number of edges (edges) and is graph directed or not (directed) (it accepts 0 or 1).
+  Followed by edges number of line consisting u v w where
+    u: start node
+    v: end node
+    w: weight of edge
+  Followed by a and b:
+    a: start node
+    b: end node
+
+Output:
+  adjacent matrix followed by 
+  Shortest distance between a and b.
+
+Sample input-output:
+  Directed graph
+  Input1:
+    4 5 1
+    1 2 5
+    2 3 7
+    2 4 6
+    3 4 5
+    4 1 2
+    3 2
+
+  Output1:
+    0 5 12 11
+    8 0 7 6
+    7 12 0 5
+    2 7 14 0
+    Shortest distance between 3 and 2 is 12
+
+  Undirected graph
+  Input2:
+    5 6 0
+    1 2 5
+    1 4 9
+    1 5 1
+    2 3 2
+    3 4 7
+    4 5 2
+    5 3
+
+  Output2:
+    0 5 7 3 1 
+    5 0 2 8 6 
+    7 2 0 7 8 
+    3 8 7 0 2 
+    1 6 8 2 0 
+    Shortest distance between 5 and 3 is 8
+
+  Time complexity = O (n^3)
+  Space complexity = O (n^2)
+
+  Contributed by: Hitansh K Doshi (github id: Hitansh159)
+*/
