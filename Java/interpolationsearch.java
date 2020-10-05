6642a32d666882bd22aaffaf7c7f74053804ee65
divyanshkhatana <44130452+divyanshkhatana@users.noreply.github.com>
2020-10-06 01:27:09 +0530

    Update interpolationsearch.java

--- Java/interpolationsearch.java
+++ Java/interpolationsearch.java
@@ -68,4 +68,7 @@ class interpolationsearch
   
   Output:
 Element found at index 4
+
+Time Complexity: If elements are uniformly distributed, then O (log log n)). In worst case it can take upto O(n).
+Auxiliary Space: O(1)
 */
