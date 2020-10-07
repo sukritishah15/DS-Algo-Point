234be05afd251d467a28bbc1c70b0b496261a832
cyenney <46497569+cyenney@users.noreply.github.com>
2020-10-07 13:41:08 -0400

    Add files via upload
    added CollatzSequence solution

---
+++ C#/CollatzSequence.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollatzSequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number to see the Collatz Sequence");
+            int startingNum = Convert.ToInt32(Console.ReadLine());
+
+            while (startingNum != 1)
+            {
+                Console.Write(startingNum + " ");
+
+               
+                if ((startingNum & 1) == 1)
+                    startingNum = 3 * startingNum + 1;
+
+                
+                else
+                    startingNum = startingNum / 2;
+            }
+
+           
+            Console.Write(startingNum);
+
+            Console.ReadLine();
+        } 
+
+           
+
+       
+        
+        
+    }
+}
