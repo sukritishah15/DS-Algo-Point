06ff5a41a688919a665999bda7ecf88355ed53c4
Paul Evans <7345987+PaulEvans8669@users.noreply.github.com>
2020-10-07 16:32:12 +0200

    Update CountSort.js

--- JavaScript/CountSort.js
+++ JavaScript/CountSort.js
@@ -10,3 +10,14 @@ function countSort(array){
         return acc.concat(Array.from({length:val[1]}).map(x => parseInt(val[0])));
     }, []);
 }
+
+/* Example
+ * Input:
+ *    Unsorted Array: Array - e.g.: [ 5,4,7,3,1,8,2,6 ]
+ *
+ * Output:
+ *    [1,2,3,4,5,6,7,8]
+ *
+ * Time Complexity: O(n)
+ * Space Complexity: O(1)
+*/
