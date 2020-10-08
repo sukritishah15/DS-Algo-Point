333422e450bf5aa1627ba55c239b37d2d87d2896
siddharth009 <spaudel206@gmail.com>
2020-10-08 08:39:12 +0545

    knapsack alogorith improved (#530)

--- JavaScript/knapSack.js
+++ JavaScript/knapSack.js
@@ -1,4 +1,4 @@
-const knapSack = (items, capacity) => {
+const knapSackSolution = (items, totalCapacity) => {
     var singleItem = 0,
         singleItemWeight = 0,
         oldMax = 0,
@@ -10,14 +10,14 @@ const knapSack = (items, capacity) => {
 
     for (singleItem = 0; singleItem < numItems + 1; singleItem++)
     {
-        weightMatrix[singleItem] = new Array(capacity + 1);
-        tempMatrix[singleItem] = new Array(capacity + 1);
+        weightMatrix[singleItem] = new Array(totalCapacity + 1);
+        tempMatrix[singleItem] = new Array(totalCapacity + 1);
     }
 
 
     for (singleItem = 0; singleItem <= numItems; singleItem++)
     {
-        for (singleItemWeight = 0; singleItemWeight <= capacity; singleItemWeight++)
+        for (singleItemWeight = 0; singleItemWeight <= totalCapacity; singleItemWeight++)
         {
 
             if (singleItem === 0 || singleItemWeight === 0)
@@ -26,9 +26,9 @@ const knapSack = (items, capacity) => {
             }
 
 
-            else if (items[singleItem - 1].w <= singleItemWeight)
+            else if (items[singleItem - 1].weight <= singleItemWeight)
             {
-                newMax = items[singleItem - 1].b + weightMatrix[singleItem - 1][singleItemWeight - items[singleItem - 1].w];
+                newMax = items[singleItem - 1].index + weightMatrix[singleItem - 1][singleItemWeight - items[singleItem - 1].weight];
                 oldMax = weightMatrix[singleItem - 1][singleItemWeight];
 
 
@@ -52,34 +52,42 @@ const knapSack = (items, capacity) => {
     }
 
     // creating solution set
-    singleItemWeight = capacity;
+    singleItemWeight = totalCapacity;
     singleItem = numItems;
     for (singleItem; singleItem > 0; singleItem--)
     {
         if (tempMatrix[singleItem][singleItemWeight] === 1)
         {
             solutionSet.push(items[singleItem - 1]);
-            singleItemWeight = singleItemWeight - items[singleItem - 1].w;
+            singleItemWeight = singleItemWeight - items[singleItem - 1].weight;
         }
     }
-    return { "Maximum Value": weightMatrix[numItems][capacity], "Solution Set": solutionSet };
+    return { "Maximum Value": weightMatrix[numItems][totalCapacity], "Solution Set": solutionSet };
 }
 
-// Example
+
+/*  Name of Algorithm : Kadane's Algorithm
+Time Complexity : O(n)
+Space Complexity : O(1)
+The first argument is the array of objects with weight and items, second argument is 
+the total capacity of the bag.  */
+
 
 const items = [{
-    "w": 5,
-    "b": 1,
+    "weight": 5,
+    "index": 1,
 
 },
 {
-    "w": 10,
-    "b": 2,
+    "weight": 10,
+    "index": 2,
 },
 {
-    "w": 15,
-    "b": 3,
+    "weight": 15,
+    "index": 3,
 }
 ]
-let value = knapSack(items, 15);
+let value = knapSackSolution(items, 15);
 console.log(value);
+
+Output: 
