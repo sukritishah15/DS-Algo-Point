04ab76ca46ae3301a456d61f2d8dddcb868fa436
aniketprasadgit <69252032+aniketprasadgit@users.noreply.github.com>
2020-10-15 23:51:31 +0530

    Single level inheritance using C++.
    multiplication of two numbers using single level inheritance in C++.

---
+++ C-Plus-Plus/inheritance.cpp
@@ -0,0 +1,30 @@
+#include <iostream>  
+using namespace std;  
+class A  
+{  
+    int a = 4;  
+    int b = 5;  
+    public:  
+    int mul()  
+    {  
+        int c = a*b;  
+        return c;  
+    }     
+};  
+  
+class B : private A  
+{  
+    public:  
+    void display()  
+    {  
+        int result = mul();  
+        std::cout <<"Multiplication of a and b is : "<<result<< std::endl;  
+    }  
+};  
+int main()  
+{  
+   B b;  
+   b.display();  
+  
+    return 0;  
+}  
