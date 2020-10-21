213db031f61b8fadb15669dd43c4be2551361c80
alesnim@gmail.com <alesnim@gmail.com>
2020-10-21 08:31:47 +0300

    patch function and add time|space complexity

--- Kotlin/PalindromeCheck.kt
+++ Kotlin/PalindromeCheck.kt
@@ -1,10 +1,32 @@
 fun main() {
     val aWord: String = scanner.next()
-    val anotherWord: String = scanner.next()
 
-    println(message = aWord isPalindromeOf anotherWord)
+    println(message = aWord.isPalindromeOf())
 
 }
 
 //* just compare reversed string and not reversed
-private infix fun String.isPalindromeOf(anotherWord: String): Boolean = this.reversed() == anotherWord.toString()
+private fun String.isPalindromeOf(): Boolean = this.reversed() == this
+
+
+/*
+* Input:
+* - a word
+*
+* Output:
+* Print "true" if  word are palindrome; otherwise, print "false" instead.
+*
+* Example 1:
+*
+* Input 1:
+* - level
+* Output 1: true
+*
+* Input 2:
+* - hello
+* Output 2: false
+*
+* Time Complexity : O(N), N the length of word.
+* Space Complexity : O(2N), N the length of words.
+*
+ */
